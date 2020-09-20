2420e13be63f5e4ef5e77ed4b15afb93abd4b84f
dievlone <53396060+dievlone@users.noreply.github.com>
2020-09-19 23:45:03 -0700

    Change photo uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'photo/index' => 'photo#index'
-  get 'user/index' => 'user#index'
+  get 'photos/index/:id' => 'photo#index', as: 'user_photos'
+  get 'users/index' => 'user#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
